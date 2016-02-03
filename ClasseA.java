95b2324b57882fdec200c55b058a1930ac782aac
luiz felipe <luizfelipe_p@hotmail.com>
2016-02-03 04:00:54 -0200

    versão 1.2 no novoRamo

--- ClasseA.java
+++ ClasseA.java
@@ -2,12 +2,22 @@ public class classA {
 	int A1;
 	float A2;
 	string A3;
+	String Aprinc4
+	
+	public string getAprinc4() {
+		return Aprinc4;
+	}
+	public void setApring4(string aprinc4) {
+		Aprinc4 = aprinc4;
+	}
+	
 	public string getA1() {
 		return A3;
 	}
 	public void setA1(string a3) {
 		A3 = a3;
 	}
+	
 	public int getA1() {
 		return A1;
 	}
@@ -20,8 +30,7 @@ public class classA {
 	public void setA2(float a2) {
 		A2 = a2;
 	}
-
-	public float soma(int, float){
+		public float soma(int, float){
 		return this.A1+this.A2;
 	}
 }
